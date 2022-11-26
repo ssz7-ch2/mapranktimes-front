dc2866120421807278461b971923fe5c5596f239
sometimes <76718358+ssz7-ch2@users.noreply.github.com>
2022-11-26 09:20:00 -0500

    Update url

--- pages/api/beatmapsets.js
+++ pages/api/beatmapsets.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const handler = async (req, res) => {
   const result = await axios.get(
-    "https://map-rank-times.onrender.com/beatmapsets"
+    "https://mapranktimes.onrender.com/beatmapsets"
   );
   res.status(200).json(result.data);
 };
