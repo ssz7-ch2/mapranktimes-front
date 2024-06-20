00ca4335bea4fd5cc2701aeabd8efba41fece7ea
ssz7.ch2 <ssz7.ch2@gmail.com>
2024-06-20 18:30:26 -0400

    remove speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
-      <SpeedInsights />
     </>
   );
 }
