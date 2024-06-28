bace2ac6f13aca967e4238e4b9aa064faaf589a6
ssz7.ch2 <ssz7.ch2@gmail.com>
2024-06-28 12:15:41 -0400

    add cache-control

--- pages/api/getupdated.js
+++ pages/api/getupdated.js
@@ -2,6 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import { Redis } from "@upstash/redis";
 
 const handler = async (req, res) => {
+  res.setHeader("Cache-Control", "max-age=50, s-maxage=50");
+
   const { updated_maps: updatedMaps, timestamp } = req.body;
 
   if (!timestamp) {
@@ -9,11 +11,12 @@ const handler = async (req, res) => {
     return;
   }
 
+  console.log("getupdated");
+
   const redis = Redis.fromEnv();
   const dataCached = await redis.get(`updates-${timestamp}`);
 
   if (dataCached) {
-    console.log("cache hit");
     res.status(200).json(typeof dataCached === "string" ? JSON.parse(dataCached) : dataCached);
     return;
   }
@@ -24,8 +27,6 @@ const handler = async (req, res) => {
   );
 
   const { data, error } = await supabase.from("beatmapsets").select("*").in("id", updatedMaps);
-
-  console.log("cache miss");
 
   if (!data || error) {
     res.status(404).json({ error: `Failed to get updated maps.` });
